30f623afe34f44274820bcbc81a3df60ce9b2036
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-03-01 21:02:49 +0100

    Add css to my leaderboard

--- src/index.js
+++ src/index.js
@@ -3,37 +3,3 @@ import './style.css';
 
 
 
-const Intro = document.querySelector('.intro')
-const myIcon = new Image();
- myIcon.src = Circle;
- console.log(myIcon);
-// Intro.insertAdjacentHTML = ('beforeend',myIcon)
-Intro.appendChild(myIcon)
-
-// import img from './img/circle.png'
-
-
-//  function component() {
-//    const element = document.createElement('div');
-//   const btn = document.createElement('button');
-
-//    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-//    element.classList.add('hello');
-
-//    // Add the image to our existing div.
-//   const myIcon = new Image();
-//   myIcon.src = Circle;
-
-//   element.appendChild(myIcon);
-
-
-
-//   btn.innerHTML = 'Click me and check the console!';
-//   btn.onclick = printMe;
-
-//   element.appendChild(btn);
-
-//    return element;
-//  }
-
-//  document.body.appendChild(component());
