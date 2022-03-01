39ec287b445c8e73bcc971698e6227da0855c9ab
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-03-01 21:14:37 +0100

    Add objects of names and scores in javascript

---
+++ src/modules/scores.js
@@ -0,0 +1,9 @@
+const scores = [
+    {Name:'Charles', score: 100},
+    {Name: 'Stanley' ,score: 502},
+    {Name: 'justin', score: 200},
+    {Name: 'Chidera', score: 50},
+    {Name: 'Nasa',score: 12}
+]
+
+export default scores;
