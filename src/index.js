82e5423d61459a926d11c33eef46abb066730023
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-03-03 09:20:08 +0100

    Fix lint errors

--- src/index.js
+++ src/index.js
@@ -1,37 +1,34 @@
 import './style.css';
-import {getScores, postScores} from './modules/api-Call.js';
+import { getScores, postScores } from './modules/api-Call.js';
 
 const displayScores = () => {
-    getScores().then((scores) => {
-        const newScores = document.querySelector('.new-scores');
-        newScores.innerHTML = '';
-  scores.result.forEach((el) => {
-    const li = document.createElement('li');
-    li.textContent =`${el.user}: ${el.score}`;
-    newScores.appendChild(li);
-    console.log(newScores);
+  getScores().then((scores) => {
+    const newScores = document.querySelector('.new-scores');
+    newScores.innerHTML = '';
+    scores.result.forEach((el) => {
+      const li = document.createElement('li');
+      li.textContent = `${el.user}: ${el.score}`;
+      newScores.appendChild(li);
+    });
+    return scores;
   });
-  return scores;
-    })
-  };
-  
+};
+
 const addUserScore = document.querySelector('.submit');
 const refresh = document.querySelector('.refresh');
 
 addUserScore.addEventListener('click', (e) => {
-    e.preventDefault();
-    const nameInput = document.querySelector('.name-input').value;
-    const scoreInput = document.querySelector('.score-input').value;
-    if (nameInput && scoreInput) {
-      postScores(nameInput, scoreInput);
-     displayScores();
-     document.querySelector('.name-input').value 
-     = document.querySelector('.score-input').value ='';
-    }
-  });
+  e.preventDefault();
+  const nameInput = document.querySelector('.name-input').value;
+  const scoreInput = document.querySelector('.score-input').value;
+  if (nameInput && scoreInput) {
+    postScores(nameInput, scoreInput);
+    displayScores();
+    document.querySelector('.name-input').value = '';
+    document.querySelector('.score-input').value = '';
+  }
+});
 
-
-
-  refresh.addEventListener('click',(e) => {
-      displayScores();
-  })
+refresh.addEventListener('click', () => {
+  displayScores();
+});
--- src/modules/api-Call.js
+++ src/modules/api-Call.js
@@ -1,26 +1,24 @@
+const getScores = async () => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7kcNOnGRPhN5KbkHWsGs/scores/';
+  const response = await fetch(url);
+  const scores = response.json();
+  return scores;
+};
 
-const getScores = async() => {
-    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7kcNOnGRPhN5KbkHWsGs/scores/';
-    const response = await fetch(url);
-    const scores = response.json() 
-    return scores;
-  }
-   
-  const postScores = async(name, points)=> {
-    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7kcNOnGRPhN5KbkHWsGs/scores/';
-    const response = await fetch(url,{
-        method: 'POST',
-        body: JSON.stringify({
-            user: name,
-            score: points,
-          }),        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-    const scores = response.json();
-    return scores;
-  };
- 
-  
-  export { getScores, postScores };
-  
+const postScores = async (name, points) => {
+  const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/7kcNOnGRPhN5KbkHWsGs/scores/';
+  const response = await fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: name,
+      score: points,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+  const scores = response.json();
+  return scores;
+};
+
+export { getScores, postScores };
