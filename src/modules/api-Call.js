42150ea2bee7aab68e29c334580f4faa6f78957b
chukwuebukaVictor <chukwuebuka.victor.ozoede@gmail.com>
2022-03-03 02:31:23 +0100

    Fetch and post Api

--- src/modules/api-Call.js
+++ src/modules/api-Call.js
@@ -1,1 +1,26 @@
 
+const getScores = async() => {
+    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OYKtMRSrWfyHNCTzyiXp/scores/';
+    const response = await fetch(url);
+    const scores = response.json() 
+    return scores;
+  }
+  
+  export const postScores = async(name, points)=> {
+    const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/OYKtMRSrWfyHNCTzyiXp/scores/';
+    const response = await fetch(url,{
+        method: 'POST',
+        body: JSON.stringify({
+            user: name,
+            score: points,
+          }),        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+        },
+    })
+    const scores = response.json();
+    return scores;
+  };
+  
+  export default getScores;
+  // export {getScores, postScores};
+  
